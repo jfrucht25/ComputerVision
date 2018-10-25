7afb94c0fd89ce5814d0a4b473dfab1dc1dc2afe
James Frucht <jfrucht25@gmail.com>
2018-10-25 18:48:39 -0400

    delete folders whose names were changed

--- Project2ConvexHull/generate_graham_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm graham_time.txt
-for i in {100..200000..100};
-do
-	echo $i
-	/usr/bin/time -f "%U" --append --output="graham_time.txt" ./graham $i > /dev/null
-done
--- Project2ConvexHull/generate_triangle_data.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm triangle_time.txt
-for i in {10..200..10};
-do
-	echo $i
-	/usr/bin/time -f "%U" --append --output="triangle_time.txt" ./triangle $i > /dev/null
-done
